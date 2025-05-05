0dce7ad6c3c985a18a7ea8504dc8380df9cfac68
Savchenko Veronika <klepa300@ya.ru>
2025-05-05 15:28:27 +0300

    LAB17 with dop

--- src/main/java/ru/hpclab/hl/additional/crach/CoreCrashScheduler.java
+++ src/main/java/ru/hpclab/hl/additional/crach/CoreCrashScheduler.java
@@ -1,7 +1,6 @@
 package ru.hpclab.hl.additional.crach;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +9,8 @@ import org.springframework.stereotype.Component;
 public class CoreCrashScheduler {
     private final WebApiKillerClient webApiKillerClient;
 
-    @Async(value = "applicationTaskExecutor")
     @Scheduled(fixedDelayString = "${service.scheduler.core.crash.delay}")
     public void callCrashEndpoint() {
-        try {
-            webApiKillerClient.crashCoreService();
-            System.out.println("Crash request sent to Core Service");
-        } catch (Exception e) {
-            System.err.println("Failed to call crash endpoint: " + e.getMessage());
-        }
+        webApiKillerClient.crashCoreService();
     }
 }
